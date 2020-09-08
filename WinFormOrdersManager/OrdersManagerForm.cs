327b4e61e738f54ba1fbbc8dd785c608f1b51e95
taurimenas <taurimenas01@gmail.com>
2020-09-08 19:10:00 +0300

    Refactored code and added sample data

--- WinFormOrdersManager/OrdersManagerForm.cs
+++ WinFormOrdersManager/OrdersManagerForm.cs
@@ -1,13 +1,6 @@
 ﻿using OrdersManager.Library;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
@@ -21,7 +14,19 @@ namespace WinFormOrdersManager
         {
             InitializeComponent();
             LoadOrders();
+            LoadSampleData();
             RefreshScreen();
+        }
+
+        private void LoadSampleData()
+        {
+            if (orders.Count < 1)
+            {
+                SqliteDataAccess.AddOrder(new Order { Address = "White Oak, 1989  Coulter Lane", Delivery = new DateTime(2020, 11, 25), Receiver = "Dustin Brown" });
+                SqliteDataAccess.AddOrder(new Order { Address = "Denver, 2178  Logan Lane", Delivery = new DateTime(2020, 10, 15), Receiver = "Linda Hill" });
+                SqliteDataAccess.AddOrder(new Order { Address = "COMSTOCK, 3494  Loving Acres Road", Delivery = new DateTime(2021, 09, 18), Receiver = "Janice Torres" });
+                LoadOrders();
+            }
         }
 
         private void LoadOrders()
@@ -38,17 +43,7 @@ namespace WinFormOrdersManager
         private void AddButton_Click(object sender, EventArgs e)
         {
             Order order = new Order();
-
-            bool deliveryIsValid = DateTime.TryParse(textBoxDelivery.Text, out DateTime delivery); //TODO perkelt
-            if (deliveryIsValid)
-                order.Delivery = delivery;
-            bool quantityIsValid = int.TryParse(textBoxQuantity.Text, out int quantity);
-            if (quantityIsValid)
-                order.Quantity = quantity;
-            order.Address = textBoxAddress.Text;
-            order.Receiver = textBoxReceiver.Text;
-            order.SelfPickUp = checkBoxSelfPickUp.Checked;
-
+            TextBoxesToOrderMapping(order);
             SqliteDataAccess.AddOrder(order);
             LoadOrders();
             RefreshScreen();
@@ -62,11 +57,37 @@ namespace WinFormOrdersManager
             RefreshScreen();
         }
 
-
-
         private void DataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             var selectedOrder = GetSelectedOrder();
+            OrderToTextBoxesMapping(selectedOrder);
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            var selectedOrder = GetSelectedOrder();
+            TextBoxesToOrderMapping(selectedOrder);
+            selectedOrder.Edited = DateTime.UtcNow;
+            SqliteDataAccess.EditOrder(selectedOrder);
+            LoadOrders();
+            RefreshScreen();
+        }
+
+        private void TextBoxesToOrderMapping(Order order)
+        {
+            bool deliveryIsValid = DateTime.TryParse(textBoxDelivery.Text, out DateTime delivery);
+            if (deliveryIsValid)
+                order.Delivery = delivery;
+            bool quantityIsValid = int.TryParse(textBoxQuantity.Text, out int quantity);
+            if (quantityIsValid)
+                order.Quantity = quantity;
+            order.Address = textBoxAddress.Text;
+            order.Receiver = textBoxReceiver.Text;
+            order.SelfPickUp = checkBoxSelfPickUp.Checked;
+        }
+
+        private void OrderToTextBoxesMapping(Order selectedOrder)
+        {
             textBoxDelivery.Text = selectedOrder.Delivery.ToString();
             textBoxAddress.Text = selectedOrder.Address;
             textBoxQuantity.Text = selectedOrder.Quantity.ToString();
@@ -74,25 +95,6 @@ namespace WinFormOrdersManager
             checkBoxSelfPickUp.Checked = selectedOrder.SelfPickUp;
         }
 
-        private void EditButton_Click(object sender, EventArgs e)
-        {
-            var selectedOrder = GetSelectedOrder();
-
-            bool deliveryIsValid = DateTime.TryParse(textBoxDelivery.Text, out DateTime delivery); //TODO perkelt
-            if (deliveryIsValid)
-                selectedOrder.Delivery = delivery;
-            bool quantityIsValid = int.TryParse(textBoxQuantity.Text, out int quantity);
-            if (quantityIsValid)
-                selectedOrder.Quantity = quantity;
-            selectedOrder.Address = textBoxAddress.Text;
-            selectedOrder.Receiver = textBoxReceiver.Text;
-            selectedOrder.SelfPickUp = checkBoxSelfPickUp.Checked;
-            selectedOrder.Edited = DateTime.UtcNow;
-
-            SqliteDataAccess.EditOrder(selectedOrder);
-            LoadOrders();
-            RefreshScreen();
-        }
         private Order GetSelectedOrder()
         {
             return dataGridView.SelectedRows[0].DataBoundItem as Order;
