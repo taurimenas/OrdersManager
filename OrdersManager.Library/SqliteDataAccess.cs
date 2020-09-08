dfe61cf5663585f035a4b84ea2c04c9cd3a7f024
taurimenas <taurimenas01@gmail.com>
2020-09-08 17:37:56 +0300

    Sqlite with Edit/Add/Delete functions

---
+++ OrdersManager.Library/SqliteDataAccess.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersManager.Library
+{
+    public class SqliteDataAccess
+    {
+        public static List<Order> LoadOrders()
+        {
+            using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = connection.Query<Order>("select * from Orders", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void EditOrder(Order order)
+        {
+            using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                connection.Execute("update Orders set Edited = @Edited, Delivery = @Delivery, Receiver = @Receiver, Address = @Address, Quantity = @Quantity, SelfPickUp = @SelfPickUp where Id = @Id", order);
+            }
+        }
+        public static void AddOrder(Order order)
+        {
+            using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                connection.Execute("insert into Orders (Created, Edited, Delivery, Receiver, Address, Quantity, SelfPickUp) values (@Created, @Edited, @Delivery, @Receiver, @Address, @Quantity, @SelfPickUp)", order);
+            }
+        }
+        public static void DeleteOrder(Order order)
+        {
+            using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                connection.Execute("delete from Orders where Id = @Id", order);
+            }
+        }
+        private static string LoadConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+        }
+    }
+}
