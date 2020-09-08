9b731f7bb898e61df04318de70e02b89b4ad1d4a
taurimenas <taurimenas01@gmail.com>
2020-09-08 10:36:55 +0300

    Added DataGridView Table

--- WinFormOrdersManager/Form1.Designer.cs
+++ WinFormOrdersManager/Form1.Designer.cs
@@ -28,13 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Created = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edited = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Delivery = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Receiver = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SelfPickUp = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Created,
+            this.Edited,
+            this.Delivery,
+            this.Receiver,
+            this.Address,
+            this.Quantity,
+            this.SelfPickUp,
+            this.Delete});
+            this.dataGridView1.Location = new System.Drawing.Point(301, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(635, 419);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 30;
+            // 
+            // Created
+            // 
+            this.Created.HeaderText = "Created";
+            this.Created.Name = "Created";
+            this.Created.ReadOnly = true;
+            this.Created.Width = 50;
+            // 
+            // Edited
+            // 
+            this.Edited.HeaderText = "Edited";
+            this.Edited.Name = "Edited";
+            this.Edited.ReadOnly = true;
+            this.Edited.Width = 50;
+            // 
+            // Delivery
+            // 
+            this.Delivery.HeaderText = "Delivery";
+            this.Delivery.Name = "Delivery";
+            this.Delivery.Width = 70;
+            // 
+            // Receiver
+            // 
+            this.Receiver.HeaderText = "Receiver";
+            this.Receiver.Name = "Receiver";
+            this.Receiver.Width = 70;
+            // 
+            // Address
+            // 
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            this.Address.Width = 150;
+            // 
+            // Quantity
+            // 
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Width = 50;
+            // 
+            // SelfPickUp
+            // 
+            this.SelfPickUp.HeaderText = "SelfPickUp";
+            this.SelfPickUp.Name = "SelfPickUp";
+            this.SelfPickUp.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.SelfPickUp.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.SelfPickUp.Width = 70;
+            // 
+            // Delete
+            // 
+            this.Delete.HeaderText = "Delete";
+            this.Delete.Name = "Delete";
+            this.Delete.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Delete.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Delete.Width = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1262, 660);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Created;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edited;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Delivery;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Receiver;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn SelfPickUp;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
     }
 }
 
