d8ec1f6626b61a53aad29596e09229d3e1a9f93a
taurimenas <taurimenas01@gmail.com>
2020-09-08 10:47:10 +0300

    Added new project library with order class model

---
+++ OrdersManager.Library/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersManager.Library
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime Edited { get; set; } = DateTime.UtcNow;
+        public string Delivery { get; set; }
+        public string Reveiver { get; set; }
+        public string Address { get; set; }
+        public int Quantity { get; set; } = 1;
+        public bool SelfPickUp { get; set; }
+    }
+}
